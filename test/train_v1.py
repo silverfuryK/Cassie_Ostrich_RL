2ff3f370835c5d0159da292598a29c1ec222b6b9
Kanishk <k.kanishk001@gmail.com>
2022-11-07 01:48:32 -0500

    testing the cassie-mujoo-sim without extra files

---
+++ test/train_v1.py
@@ -0,0 +1,75 @@
+### test script for training
+import time
+import math
+from datetime import datetime
+import numpy as np
+from new_env import CassieEnv
+from ddpg_ac import Agent
+from a_c_test_agent import NewAgent
+from cassiemujoco import *
+
+model = 'cassie.xml'
+traj_path = 'ostrichrl/ostrichrl/assets/mocap/cassie/'
+bot = CassieSim(model,terrain = False)
+
+env = CassieEnv(model,traj_path,60)
+
+
+agent = Agent(alpha=0.0025, beta=0.0025, input_dims=[35], tau=0.001, env=env,
+              batch_size=64,  layer1_size=512, layer2_size=512, n_actions=20)
+#agent.load_models()
+#agent.check_actor_params()
+'''
+try: 
+        agent.load_models()
+        agent.check_actor_params()
+except:
+        print("no saved models found!!")
+'''
+score_history = []
+i = 0
+tot_episodes = 1
+max_tp = 60*60*10*10*2
+tp = 0
+for i in range(tot_episodes):
+        obs = env.reset()
+        done = False
+        score = 0
+        while tp != max_tp:
+                '''
+                cmd_vel = trajec.get_cmd_vel(sim_time)
+                #print(cmd_vel)
+                #env.action([0,0,0.1,0,0,0.1])
+                action = np.array(agent.choose_action(observation)).reshape((1,))
+                print(action)
+                #p.stepSimulation()
+                observation_, reward, done = env.step_simulation(action)
+                agent.learn(observation, reward, observation_, done)
+                observation = observation_
+                #print(done)
+                #print([env.obs_t[2],env.obs_t[3],env.obs_t[6],env.obs_t[7],env.obs_t[11]])
+                score += reward
+                #time.sleep(dt)
+                '''
+                act = agent.choose_action(obs)
+                new_state, reward, done = env.step(act)
+                #print(new_state)
+                agent.remember(obs, act, reward, new_state, int(done))
+                agent.learn()
+                score += reward
+                obs = new_state
+                #time.sleep(dt)
+                #env.render()
+                
+                print('timestep: ', tp,'sim time: %.2f'% env.sim_time,' reward: ',env.reward_t)
+                tp = tp + 1
+        score_history.append(score)
+        print('episode: ', i,'score: %.2f' % score,'sim time: %.2f'% env.sim_time,' reward: ',env.reward_t)
+        #print('sim time: %.2f'% env.sim_time,' reward: ',env.reward_t)
+        #print(env.obs_t, env.action)
+        #print(env.reward_t)
+print(agent.actor.checkpoint_file)
+agent.save_models()      
+#plotLearning(score_history, filename = 'plot1.png', window=20)
+print("DONE lol")
+
